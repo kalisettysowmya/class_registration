73aa2ccc79a4a5dc35f88cb7f37ac8ed7d9d3198
Sowmya Kalisetty <39836310+kalisettysowmya@users.noreply.github.com>
2025-02-05 08:53:34 -0500

    Update admin.py

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Course, Student
 
+admin.site.site_header = "Class Registration System Admin Panel"
+admin.site.site_title = "Class Registration Admin"
+admin.site.index_title = "Welcome to the Class Registration System"
+
 admin.site.register(Course)
 admin.site.register(Student)
